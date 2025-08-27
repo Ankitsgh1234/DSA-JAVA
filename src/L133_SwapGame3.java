75067a49f6e38df49c11e15c167488c4d5e95a6b
Ankitsgh1234 <ankit2005sgh@gmail.com>
2025-08-28 00:30:59 +0530

    Added Swap game 3

---
+++ src/L133_SwapGame3.java
@@ -0,0 +1,41 @@
+public class L133_SwapGame3 {
+    public static class Person{
+        int age ;
+        String name;
+
+        void saysHi(){
+            System.out.println(name+"["+age+"] says hi");
+        }
+    }
+    public static void main(String[] args) {
+        Person P1=new Person();
+        Person P2=new Person();
+        P1.name="A";
+        P1.age=19;
+        
+        P2.name="B";
+        P2.age=120;
+
+        P1.saysHi();
+        P2.saysHi();
+        Swap(P1, P2);
+        P1.saysHi();
+        P2.saysHi();
+    }
+    
+    public static void Swap(Person p1,Person p2)
+    {
+        p1=new Person();
+        int age=p1.age;
+        p1.age=p2.age;
+        p2.age=age;
+        
+        p2=new Person();
+       String name=p1.name;
+       p1.name=p2.name;
+       p2.name=name;
+
+
+    }
+
+}
