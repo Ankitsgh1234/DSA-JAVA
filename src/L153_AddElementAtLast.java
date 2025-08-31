b95a483428baa7b70c228eb54fbc86290074483e
Ankitsgh1234 <ankit2005sgh@gmail.com>
2025-09-01 01:42:36 +0530

    Correction in LL

--- src/L153_AddElementAtLast.java
+++ src/L153_AddElementAtLast.java
@@ -8,14 +8,22 @@ public class L153_AddElementAtLast {
         Node head;
         Node tail;
 
-        void addLast(in val){
+        void addLast(int val){
             if(size==0){
                 Node temp=new Node();
                 temp.data=val;
                 temp.next=null;
                 head=tail=temp;
                 size++;
-                
+
+            }
+            else{
+                Node temp=new Node();
+                temp.data=val;
+                tail.next=temp;
+                tail=temp;
+                size++; 
+
             }
         }
 
